79c6dcd4bdf577a3e08da9559e92f9c89a133692
Mark Quinn <mq9117@gmail.com>
2018-09-16 02:35:31 +0200

    Initial commit

--- 8Ball/ViewController.swift
+++ 8Ball/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var ballIndex : Int = 0
+    let ballImagesArray  = ["ball1","ball2","ball3","ball4","ball5"]
+    
+    @IBOutlet weak var ballImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func shakeButtonPressed(_ sender: UIButton) {
+    
+    updateBallImage()
+    
+    }
+    
+    func updateBallImage(){
+        ballIndex = Int(arc4random_uniform(5))
+        ballImage.image = UIImage(named : ballImagesArray[ballIndex])
+        
+    }
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateBallImage()
+    }
 }
 
